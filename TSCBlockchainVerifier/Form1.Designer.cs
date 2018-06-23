624d6ef4a1984bec56e9955a6c22f45fa9c64511
Utharn Buranasaksee <zarimpun@msn.com>
2018-06-23 22:26:54 +0700

    Initial Release

--- TSCBlockchainVerifier/Form1.Designer.cs
+++ TSCBlockchainVerifier/Form1.Designer.cs
@@ -28,13 +28,216 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.FileLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SignTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TX1TextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TX2TextBox = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SignatureTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ResultTextBox = new System.Windows.Forms.TextBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "All files|*.*";
+            this.openFileDialog1.Title = "เลือกแฟ้มข้อมูล";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.button1.Location = new System.Drawing.Point(29, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 42);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "1. เลือกไฟล์";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Coral;
+            this.label1.Location = new System.Drawing.Point(231, 155);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "...";
+            // 
+            // FileLabel
+            // 
+            this.FileLabel.AutoSize = true;
+            this.FileLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.FileLabel.Location = new System.Drawing.Point(208, 35);
+            this.FileLabel.Name = "FileLabel";
+            this.FileLabel.Size = new System.Drawing.Size(25, 24);
+            this.FileLabel.TabIndex = 3;
+            this.FileLabel.Text = "...";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label3.Location = new System.Drawing.Point(24, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(182, 29);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "2. ข้อความเกษียณ";
+            // 
+            // SignTextBox
+            // 
+            this.SignTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.SignTextBox.Location = new System.Drawing.Point(212, 77);
+            this.SignTextBox.MaxLength = 512;
+            this.SignTextBox.Name = "SignTextBox";
+            this.SignTextBox.Size = new System.Drawing.Size(770, 32);
+            this.SignTextBox.TabIndex = 2;
+            this.SignTextBox.TextChanged += new System.EventHandler(this.SignTextBox_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label4.Location = new System.Drawing.Point(24, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(170, 29);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "3.รหัสธุรกรรม 1";
+            // 
+            // TX1TextBox
+            // 
+            this.TX1TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.TX1TextBox.Location = new System.Drawing.Point(212, 122);
+            this.TX1TextBox.MaxLength = 512;
+            this.TX1TextBox.Name = "TX1TextBox";
+            this.TX1TextBox.Size = new System.Drawing.Size(770, 32);
+            this.TX1TextBox.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label5.Location = new System.Drawing.Point(24, 171);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(170, 29);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "4.รหัสธุรกรรม 2";
+            // 
+            // TX2TextBox
+            // 
+            this.TX2TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.TX2TextBox.Location = new System.Drawing.Point(212, 168);
+            this.TX2TextBox.MaxLength = 512;
+            this.TX2TextBox.Name = "TX2TextBox";
+            this.TX2TextBox.Size = new System.Drawing.Size(770, 32);
+            this.TX2TextBox.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.button2.Location = new System.Drawing.Point(367, 271);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(271, 42);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "ตรวจสอบ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // SignatureTextBox
+            // 
+            this.SignatureTextBox.BackColor = System.Drawing.Color.White;
+            this.SignatureTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.SignatureTextBox.Location = new System.Drawing.Point(212, 222);
+            this.SignatureTextBox.MaxLength = 512;
+            this.SignatureTextBox.Name = "SignatureTextBox";
+            this.SignatureTextBox.ReadOnly = true;
+            this.SignatureTextBox.Size = new System.Drawing.Size(770, 32);
+            this.SignatureTextBox.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label2.Location = new System.Drawing.Point(24, 225);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 29);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "ลายเซ็นดิจิทัล";
+            // 
+            // ResultTextBox
+            // 
+            this.ResultTextBox.BackColor = System.Drawing.Color.White;
+            this.ResultTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.ResultTextBox.Location = new System.Drawing.Point(60, 326);
+            this.ResultTextBox.MaxLength = 512;
+            this.ResultTextBox.Multiline = true;
+            this.ResultTextBox.Name = "ResultTextBox";
+            this.ResultTextBox.ReadOnly = true;
+            this.ResultTextBox.Size = new System.Drawing.Size(867, 98);
+            this.ResultTextBox.TabIndex = 12;
+            this.ResultTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(811, 440);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(171, 13);
+            this.linkLabel1.TabIndex = 13;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "ตรวจสอบข้อมูลบล็อกเชนด้วยตัวเอง";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(994, 462);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.ResultTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.SignatureTextBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.TX2TextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.TX1TextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.SignTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.FileLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Thaismartcontract Blockchain Verifier";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label FileLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox SignTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TX1TextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox TX2TextBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox SignatureTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox ResultTextBox;
+        private System.Windows.Forms.LinkLabel linkLabel1;
     }
 }
 
--- TSCBlockchainVerifier/Form1.cs
+++ TSCBlockchainVerifier/Form1.cs
@@ -1,9 +1,15 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Org.BouncyCastle.Crypto.Digests;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,5 +22,225 @@ namespace TSCBlockchainVerifier
         {
             InitializeComponent();
         }
+        private byte[] dataHash;
+        private string finalString;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            if (!string.IsNullOrEmpty(openFileDialog1.FileName))
+            {
+                FileLabel.Text = openFileDialog1.FileName;
+                UpdateHash();
+            }
+
+
+        }
+
+        private void UpdateHash()
+        {
+            try
+            {
+                var data = File.ReadAllBytes(openFileDialog1.FileName);
+                if (SignTextBox.Text.Trim().Length > 0)
+                {
+                    var noteByte = Encoding.UTF8.GetBytes(SignTextBox.Text);
+                    var noteHash = CalculateHash(noteByte);
+                    var dataConcat = noteHash.Concat(dataHash).ToArray();
+                    dataHash = CalculateHash(dataConcat);
+                }
+                else
+                {
+                    dataHash = CalculateHash(data);
+                }
+                finalString = BitConverter.ToString(dataHash).Replace("-", "").ToLower();
+                SignatureTextBox.Text = finalString;
+            }
+            catch (Exception)
+            {
+                return;
+            }
+
+        }
+
+        public byte[] CalculateHash(byte[] data)
+        {
+            var cloner = new byte[data.Length];
+            Array.Copy(data, cloner, data.Length);
+            var hasher = new KeccakDigest(256);
+            hasher.BlockUpdate(cloner, 0, cloner.Length);
+            var output = new byte[hasher.GetDigestSize()];
+            hasher.DoFinal(output, 0);
+            return output;
+        }
+
+        private void SignTextBox_TextChanged(object sender, EventArgs e)
+        {
+            UpdateHash();
+        }
+
+        private string ServerKey = "DCzmzkMBqEz2tLn47W9YuNAV9cFzuWCydW";
+        private string Key1, newKey1;
+        private string Key2, newKey2;
+        private bool IsValid1 = false;
+        private bool IsValid2 = false;
+        private bool IsKeyValid = false;
+        private bool IsTimeValid = false;
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://digiexplorer.info");
+        }
+
+        private DateTime BlockTime, BlockTime2;
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            string resultString = "";
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.BaseAddress = new Uri("https://digiexplorer.info/api/");
+
+            try
+            {
+                var response = await client.GetAsync($"tx/{TX1TextBox.Text}");
+                response.EnsureSuccessStatusCode();
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var result = JsonConvert.DeserializeObject<RawTransaction>(responseBody);
+                if (result.vout.First(v => v.scriptPubKey.asm.Contains("OP_RETURN")).scriptPubKey.hex.Contains(finalString))
+                {
+                    IsValid1 = true;
+                    Key1 = result.vin.First().addr;
+                    Key2 = result.vout.First(v => v.scriptPubKey.asm.Contains("OP_EQUALVERIFY")).scriptPubKey.addresses.First();
+                    BlockTime = new DateTime(1970, 1, 1).AddSeconds(result.blocktime);
+                }
+            }
+            catch (Exception)
+            {
+                resultString = "";
+            }
+
+            try
+            {
+                var response2 = await client.GetAsync($"tx/{TX2TextBox.Text}");
+                response2.EnsureSuccessStatusCode();
+
+                string responseBody = await response2.Content.ReadAsStringAsync();
+                var result = JsonConvert.DeserializeObject<RawTransaction>(responseBody);
+                if (result.vout.First(v => v.scriptPubKey.asm.Contains("OP_RETURN")).scriptPubKey.hex.Contains(finalString))
+                {
+                    IsValid2 = true;
+                    newKey1 = result.vin.First().addr;
+                    newKey2 = result.vout.First(v => v.scriptPubKey.asm.Contains("OP_EQUALVERIFY")).scriptPubKey.addresses.First();
+                    BlockTime2 = new DateTime(1970, 1, 1).AddSeconds(result.blocktime);
+
+                }
+            }
+            catch (Exception)
+            {
+                resultString = "";
+            }
+
+            if (BlockTime == BlockTime2)
+            {
+                IsTimeValid = true;
+            }
+            if (Key1 == newKey2 && Key2 == newKey1)
+            {
+                IsKeyValid = true;
+            }
+            if (!IsValid1 || !IsValid2)
+            {
+                resultString += "รหัสธุรกรรมอย่างน้อย 1 ธุรกรรมไม่ตรงกับการเซ็น\r\n";
+            }
+            else
+            {
+                if (IsTimeValid)
+                {
+                    resultString += $"ข้อมูลอยู่ในบล็อกเชนเวลา {BlockTime.ToString("d MMMM yyyy HH:mm:ssน.", new System.Globalization.CultureInfo("th-TH"))}\r\n";
+                }
+
+                if (IsKeyValid)
+                {
+                    if (Key1 == ServerKey || Key2 == ServerKey)
+                    {
+                        resultString += "ธุรกรรมทำกับระบบไทยสมาร์ทคอนแทรค\r\n";
+                    }
+                    if (Key1 == ServerKey)
+                    {
+                        resultString += $"กุญแจสาธารณะของผู้เซ็นคือ {Key2}\r\n";
+                    }
+                    else
+                    {
+                        resultString += $"กุญแจสาธารณะของผู้เซ็นคือ {Key1}\r\n";
+                    }
+                }
+                else
+                {
+                    resultString += "ธุรกรรมไม่ได้กระทำโดยผู้เซ็นคนเดียวกัน\r\n";
+                }
+
+            }
+
+            ResultTextBox.Text = resultString;
+        }
+    }
+
+    public class RawTransaction
+    {
+        public string hex { get; set; }
+        public string txid { get; set; }
+        public string hash { get; set; }
+        public int size { get; set; }
+        public int vsize { get; set; }
+        public int version { get; set; }
+        public int locktime { get; set; }
+        public List<Vin> vin { get; set; }
+        public List<Vout> vout { get; set; }
+        public string blockhash { get; set; }
+        public long confirmations { get; set; }
+        public long time { get; set; }
+        public long blocktime { get; set; }
+        public decimal valueOut { get; set; }
+        public decimal valueIn { get; set; }
+        public decimal fees { get; set; }
+    }
+
+    public class Vin
+    {
+        public string coinbase { get; set; }
+        public List<string> txinwitness { get; set; }
+        public string txid { get; set; }
+        public int vout { get; set; }
+        public ScriptSig scriptSig { get; set; }
+        public long sequence { get; set; }
+        public int n { get; set; }
+        public string addr { get; set; }
+        public long valueSat { get; set; }
+        public decimal value { get; set; }
+        public object doubleSpentTxID { get; set; }
+    }
+
+    public class Vout
+    {
+        public decimal value { get; set; }
+        public int n { get; set; }
+        public ScriptPubKey scriptPubKey { get; set; }
+    }
+
+    public class ScriptSig
+    {
+        public string asm { get; set; }
+        public string hex { get; set; }
+    }
+
+    public class ScriptPubKey
+    {
+        public string asm { get; set; }
+        public string hex { get; set; }
+        public int reqSigs { get; set; }
+        public string type { get; set; }
+        public List<string> addresses { get; set; }
     }
 }
